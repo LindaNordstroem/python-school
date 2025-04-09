bc72036c5d037011932fc78416ad016896f9cf4f
LindaNordstroem <linda.noordstroom@gmail.com>
2025-04-09 13:41:20 +0200

    calculator age 100

---
+++ uppgift.py
@@ -0,0 +1,6 @@
+name = input (("Hello what is your name?" ))
+age = int(input(f"Hello {name},  how old are you?" ))
+year = 2025 - age + 100
+print(f"Hello {name}, you will turn 100 year {year}" )
+
+
